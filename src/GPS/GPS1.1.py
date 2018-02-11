ce4c0e475204f15c8323076cef93b9abdb608c80
JombartOlivier <olivier.jombart@isen.yncrea.fr>
2018-02-11 20:02:39 +0100

    update class GPS

--- src/GPS/GPS1.1.py
+++ src/GPS/GPS1.1.py
@@ -22,22 +22,17 @@ while 1:
  if x[3]==82:
  	 	
  	#NORD
- 	liste.append(x[20])
- 	liste.append(x[21])
- 	liste.append(x[22])
- 	liste.append(x[23])
-
- 	liste.append(x[25])
- 	liste.append(x[26])
- 	
+ 	for i in range(20,26) :
+ 		
+ 		if i==24 :
+ 			i=25
+ 		liste.append(x[i])
+	
  	#EST
- 	liste.append(x[33])
- 	liste.append(x[34])
- 	liste.append(x[35])
- 	liste.append(x[36])
- 	
- 	liste.append(x[38])
- 	liste.append(x[39])
+ 	for i in range(33,39) :
+ 		if i==37 :
+ 			i=38
+ 		liste.append(x[i])
  	 	
  	l=''.join(chr(i) for i in liste)
  	print(l)
--- src/GPS/GPS1.2.py
+++ src/GPS/GPS1.2.py
@@ -10,42 +10,42 @@ class GPS:
 	
 	def __init__ (self) :
 
-		self.lattitude=[]
-		self.longitude=[]
-		self._port = ""
-        	self._baudRate = 0
-       	self._timeOut = 0
-        	self._serialPort
-		
-     def _set_serialPort(self, serialPort):
-          self._serialPort
+		self._lattitude=[]
+		self._longitude=[]
+		self._port=""
+		#self._baudRate=0
+		self._timeOut=0
+		self._serialPort
 
-     def _set_port(self, port):
-          self._port = port
-    
-     def _set_baudRate(self, baudRate):
-          self._baudRate = baudRate
-    
-     def _set_timeOut(self, timeOut):
-          self._timeOut = timeOut
+	def _set_serialPort(self, serialPort):
+		self._serialPort
 
-     def _set_lattitude(self, lattitude):
-     	self._lattitude=lattitude
+	def _set_port(self, port):
+		self._port=port
 
-     def _set_longitude(self, longitude):
-     	self._longitude=longitude
+	def _set_baudRate(self, baudRate):
+		self._baudRate=baudRate
 
-     def _get_serialPort(self):
-          return self._serialPort
+	def _set_timeOut(self, timeOut):
+		self._timeOut=timeOut
 
-     def _get_port(self):
-          return self._port
+	def _set_lattitude(self, lattitude):
+		self._lattitude=lattitude
 
-     def _get_baudRate(self):
-          return self._baudRate
+	def _set_longitude(self, longitude):
+		self._longitude=longitude
 
-     def _get_timeOut(self):
-          return self._timeOut
+	def _get_serialPort(self):
+		return self._serialPort
+
+	def _get_port(self):
+		return self._port
+
+	def _get_baudRate(self):
+		return self._baudRate
+
+	def _get_timeOut(self):
+		return self._timeOut
 
 	def _get_lattitude (self):
 		return self._lattitude
@@ -54,82 +54,56 @@ class GPS:
 		return self._longitude
 		
 
-
-     port = property(_get_port, _set_port)
-    	baudRate = property(_get_baudRate, _set_baudRate)
-   	timeOut = property(_get_timeOut, _set_timeOut)
-    	serialPort = property(_get_serialPort, _set_serialPort)
-    	longitude = property(_get_longitude, _set_longitude)
-    	lattitude = property(_get_lattitude, _set_lattitude)
-
-    	def openUartPort(self, port, baudRate, timeout):
-
-        self.port = port
-        self.baudRate = baudRate
-        self.timeOut = timeout
-
-        
-        print("Parametre de la liaison Uart :")
-        print("Port       : ", self.port)
-        print("Baude rate : ", self.baudRate)
-        print("timeout   : " , self.timeOut)
-
-        self.serialPort = serial.Serial(self.port, self.baudRate, timeout = self.timeOut)
-
-    	def closeUartPort(self):
-       self.serialPort.close()
-
 	def setDataGPS() :
 
 		self.lattitude = lattitude
 		self.longitude = longitude
 		self.serialPort = ser
+		ser = serial.Serial(
+			port='/dev/ttyAMA0',
+			baudrate = 9600,
+			parity=serial.PARITY_NONE,
+			stopbits=serial.STOPBITS_ONE,
+			bytesize=serial.EIGHTBITS,
+			timeout=1
+		)
+		counter=0
 
-		liste=[]
 		while 1:
-		 x=ser.readline()
-		 if x[3]==82: #on veut la valeur du GPRMC
-		 	#GPRMC
-		 	liste.append(x[0])
-		 	liste.append(x[1])
-		 	liste.append(x[2])
-		 	liste.append(x[3])
-		 	liste.append(x[4])
-		 	liste.append(x[5])
-		 	
-		 	#lattitude
-		 	lattitude.append(x[20])
-		 	lattitude.append(x[21])
-		 	lattitude.append(x[22])
-		 	lattitude.append(x[23])
+			x=ser.readline()
 
-		 	lattitude.append(x[25])
-		 	lattitude.append(x[26])
-		 	lattitude.append(x[27])
-		 	lattitude.append(x[28])
-		 	#longitude
-		 	longitude.append(x[33])
-		 	longitude.append(x[34])
-		 	longitude.append(x[35])
-		 	longitude.append(x[36])
-		 	
-		 	longitude.append(x[38])
-		 	longitude.append(x[39])
-		 	longitude.append(x[40])
-		 	longitude.append(x[41])
-		 	
-		 	l=''.join(chr(i) for i in liste)
-		 	lattitudePrint=''.join(chr(j) for j in lattitude)
-		 	longitudePrint=''.join(chr(k)	for k in longitude)
-		 	print(l)
-		 	print(lattitude)
-		 	print(longitude)
-		 	del(l[:])
-		 	break
+			if x[3]==82:	
+		 		#NORD
+		 		for i in range(20,26) :
+		 		
+		 			if i==24 :
+		 				i=25
+		 			lattitude.append(x[i])
+			
+		 		#EST
+		 		for i in range(33,39) :
+		 			if i==37 :
+		 				i=38
+		 			longitude.append(x[i])
+		 	 	
+		 		lattitudePrint=''.join(chr(j) for j in lattitude)
+		 		longitudePrint=''.join(chr(k)	for k in longitude)
+		 		
+		 		print(lattitude)
+		 		print(longitude)
+		 		
+		 		break
+		
 		
 	def clearDataGps():
 
-	del(lattitude[:])
-	del(longitude[:])
+		del(lattitude[:])
+		del(longitude[:])
 
 
+	port = property(_get_port, _set_port)
+	baudRate = property(_get_baudRate, _set_baudRate)
+	timeOut = property(_get_timeOut, _set_timeOut)
+	serialPort = property(_get_serialPort, _set_serialPort)
+	longitude = property(_get_longitude, _set_longitude)
+	lattitude = property(_get_lattitude, _set_lattitude)
