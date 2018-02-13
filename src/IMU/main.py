344686106c4ec36a3d8bc00c3beab736ff10a7d1
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2018-02-13 15:01:59 +0100

    add IMU

---
+++ src/IMU/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import imu
+
+axeX = 0
+axeY = 0
+axeZ = 0 
+
+get_x_rotation(axeX, axeY, axeZ)
--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ a = SigfoxCom.Sigfox
 print (dir(a))
 a.openUartPort(a, "/dev/ttyAMA0", 115200, 2)
 a.wakeUpSigfox(a)
-a.sendData(a, "Hello")
+
+a.sendData(a, "Hello World")
 a.closeUartPort(a)
 #a.openUartPort(a, "/dev/ttyAMA0", 9600, 2)
 #a.receiveData(a, 11)
