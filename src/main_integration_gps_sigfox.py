904d82cb571b4e247a0d9d5a17182f256272e000
JombartOlivier <olivier.jombart@isen.yncrea.fr>
2018-02-14 16:10:50 +0100

    update new mainV2

--- src/main_integration_gps_sigfox.py
+++ src/main_integration_gps_sigfox.py
@@ -7,14 +7,13 @@ import GPS1_2
 import SigfoxCom
 a = GPS1_2.GPS
 b= SigfoxCom.Sigfox
-print (dir(a))
 a.setDataGps(a)
 a._get_lattitude(a)
 a._get_longitude(a)
 a._set_lattitude(a,"553520")
 a._set_longitude(a,"034343")
-b.openUartPort(b,"ttyAMA0",115200,2)
+b.openUartPort(b,"/dev/ttyAMA0", 115200, 2)
 b.wakeUpSigfox(b)
-b.sendData(b,a._get_lontgitude)
-b.closeUartPort()
+b.sendData(b,a._get_longitude(a))
+b.closeUartPort(b)
 
