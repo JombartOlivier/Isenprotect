1b3e6cbf99e19988d5105b4d3ca92d2a2fd739e2
ArthurDuytschaever <arthur.duytschaever@isen.yncrea.fr>
2018-02-17 10:06:19 +0100

    move files

---
+++ src/apresDetection4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3 
+#coding : utf8
+  
+import RPi.GPIO as GPIO  
+import time 
+import pygame
+
+  
+
+#flag de sortie de boucle
+accident_potentiel=0
+state=0
+    # set GPIO25 as input (button)  
+
+#init lecteur audio
+pygame.mixer.init()
+pygame.mixer.music.set_volume(2.0)
+		
+#fonction play music
+def play_music(state) :
+	
+	if state == 1:
+		path = "/home/pi/Documents/Isenprotect/src/fichiers_audio/Alerte_collision_detectee.mp3"
+	elif state == 2:
+		path = "/home/pi/Documents/Isenprotect/src/fichiers_audio/Envoie_des_secours_a_votre_position.mp3"
+	elif state == 3:
+		path3 = "/home/pi/Documents/Isenprotect/src/fichiers_audio/Fin_d_alerte.mp3"
+
+	pygame.mixer.music.load(path)
+	pygame.mixer.music.play()
+
+	while pygame.mixer.music.get_busy() == True:
+   		continue
+	
+
+
+
+
+
+  
